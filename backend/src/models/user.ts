444036cd03adc715b171534c3d8fb852a0f2d408
Digvijay Singh <dvsingh958@gmail.com>
2024-03-21 09:51:22 +0530

    user backend part

---
+++ backend/src/models/user.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+// name addressLine1 city country are not required because they are not provided on auth0 signup
+
+const User = mongoose.model("User", userSchema);
+export default User;
